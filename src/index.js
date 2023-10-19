e6ff6eaa1bdf5d6560e355d20427f815a1b5fe83
vaj573 <jhonedw573@gmail.com>
2023-10-19 02:28:36 -0500

    ajuste del java por error de mezcla x cambiar name

--- src/index.js
+++ src/index.js
@@ -1,75 +1,3 @@
-<<<<<<< HEAD
-let page = 1; // Valor inicial de la paginación
-let totalHits = 0; // Variable para almacenar el total de imágenes encontradas
-
-document.getElementById('search-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  // Resetear la página al buscar una nueva palabra clave
-  page = 1;
-
-  const searchQuery = document.querySelector('input[name="searchQuery"]').value;
-
-  // Tu clave de acceso de Pixabay
-  const apiKey = '40114076-6792fe4ed8fc0cf826901eacd';
-
-  // Construir la URL de la API de Pixabay con paginación
-  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(searchQuery)}&page=${page}&per_page=40`;
-
-  // Realizar la solicitud HTTP
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      // Limpiar la galería antes de mostrar nuevos resultados
-      clearGallery();
-
-      // Verificar si hay resultados
-      if (data.hits.length > 0) {
-        totalHits = data.totalHits; // Almacenar el total de imágenes encontradas
-        displayResults(data.hits);
-
-        // Mostrar el botón de carga más
-        document.getElementById('load-more').style.display = 'block';
-      } else {
-        displayNoResults();
-      }
-
-      // Verificar si se han alcanzado todos los resultados
-      if (data.totalHits <= page * 40) {
-        // Ocultar el botón de carga más y mostrar notificación
-        document.getElementById('load-more').style.display = 'none';
-        document.getElementById('notification').innerHTML = "We're sorry, but you've reached the end of search results.";
-      } else {
-        // Limpiar la notificación si hay más resultados
-        document.getElementById('notification').innerHTML = `Hooray! We found ${totalHits} images.`;
-      }
-    })
-    .catch(error => console.error('Error:', error));
-});
-
-document.getElementById('load-more').addEventListener('click', function () {
-  page++;
-
-  const searchQuery = document.querySelector('input[name="searchQuery"]').value;
-
-  // Tu clave de acceso de Pixabay
-  const apiKey = '40114076-6792fe4ed8fc0cf826901eacd';
-
-  // Construir la URL de la API de Pixabay con paginación
-  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(searchQuery)}&page=${page}&per_page=40`;
-
-  // Realizar la solicitud HTTP
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      // Verificar si hay resultados
-      if (data.hits.length > 0) {
-        displayResults(data.hits);
-      }
-
-      // Verificar si se han alcanzado todos los resultados
-      if (data.totalHits <= page * 40) {
-=======
 import axios, { isCancel, AxiosError } from 'axios';
 import SimpleLightbox from 'simplelightbox';
 
@@ -148,8 +76,7 @@ document.getElementById('load-more').addEventListener('click', function () {
                 displayResults(response.data.hits);
 
                 // activo scroll que desplaza las imagenes "automaticamente"
-                const { height: cardHeight } = document
-  .querySelector(".gallery")
+                const { height: cardHeight } = document.querySelector(".gallery")
   .firstElementChild.getBoundingClientRect();
 
 window.scrollBy({
@@ -159,37 +86,17 @@ window.scrollBy({
             }
 // Verificar si se han alcanzado todos los resultados
       if (response.data.totalHits <= page * 40) {
->>>>>>> a8faa0086c451545d1a5cbb4098c1c69033ae8ba
+
         // Ocultar el botón de carga más y mostrar notificación
         document.getElementById('load-more').style.display = 'none';
         document.getElementById('notification').innerHTML = "We're sorry, but you've reached the end of search results.";
       }
-<<<<<<< HEAD
+
     })
     .catch(error => console.error('Error:', error));
 });
 
-function clearGallery() {
-  const gallery = document.querySelector('.gallery');
-  gallery.innerHTML = '';
-}
-=======
 
-
-
- })
-        
-        
-      .catch(function (error) {
-        console.error('Error en la solicitud:', error);
-      });
-
-});
-
-
-
-
->>>>>>> a8faa0086c451545d1a5cbb4098c1c69033ae8ba
 
 function displayResults(images) {
 
@@ -214,18 +121,11 @@ const gallery = document.querySelector('.gallery');
 
   // Inicializar SimpleLightbox después de agregar las imágenes
   const lightbox = new SimpleLightbox('[data-lightbox="gallery"]');
-<<<<<<< HEAD
-}
 
-
-
-
-
-
-function displayNoResults() {
-  const gallery = document.querySelector('.gallery');
-  gallery.innerHTML = '<p>Sorry, there are no images matching your search query. Please try again.</p>';
-}
-=======
 };
->>>>>>> a8faa0086c451545d1a5cbb4098c1c69033ae8ba
+
+
+
+
+
+
